08bb2c0a13ffe28cd027be78737ba62e25a4f5be
USER2-80809\User <danbk88@gmail.com>
2018-10-03 14:34:32 +0300

    Setup start script and Heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+var port = process.env.PORT || 3000;
 var app = express();
 
 // Set partials pages root folder
@@ -17,18 +18,20 @@ app.use((req, res, next) => {
     console.log(log);
     fs.appendFile('server.log', log + '\n', (err) => {
         if(err){
-            console.log('Unable to append to server.log');
+            console.log('Unable to append to server.log...');
         }
     });
 
     //app will not continue if MiddleWare doesnt call next
     next();
 });
+
 // *middleware
 // Create Maintenance middleware:
 // app.use((req, res, next) => {
 //     res.render('maintenance.hbs');
 // });
+
 // *middleware
 // Create a Static location/address for app - get html files or any other resource
 app.use(express.static(__dirname + '/public'));
@@ -63,4 +66,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000);
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+});
